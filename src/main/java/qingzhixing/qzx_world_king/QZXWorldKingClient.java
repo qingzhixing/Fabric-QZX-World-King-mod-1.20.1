7d40f93df754e9551252d0677e704195e950e8d6
qingzhixing <qzsqqmail@qq.com>
2024-04-27 13:38:30 +0800

    Init

---
+++ src/main/java/qingzhixing/qzx_world_king/QZXWorldKingClient.java
@@ -0,0 +1,10 @@
+package qingzhixing.qzx_world_king;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class QZXWorldKingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/qingzhixing/qzx_world_king/QZXWorldKingDataGenerator.java
@@ -0,0 +1,11 @@
+package qingzhixing.qzx_world_king;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class QZXWorldKingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
